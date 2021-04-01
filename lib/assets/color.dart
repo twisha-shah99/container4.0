7cdea3c249be92adcb82ed03aaebccfdbe74f7eb
high-functioning-sociopath <twishas76@gmail.com>
2021-04-01 23:03:20 +0530

    Added Firebase Improved UI Details page with editing Implemented Auto-Order with SMS

--- lib/assets/color.dart
+++ lib/assets/color.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color PRIMARY_GREEN = Color(0xFF29b81c);
   static const Color PRIMARY_WHITE = Color(0xFFFFFFFF);
-  static const Color PRIMARY_LIGHT_YELLOW = Color(0xFFeffab1);
+  static const Color PRIMARY_LIGHT_YELLOW = Color(0xFFfaffcf);
+  static const Color PRIMARY_BLACK = Color(0xFF000000);
+  static const Color PRIMARY_GREY = Color(0xFFe3e1e1);
+  static const Color PRIMARY_LIGHT_GREEN = Color(0xFF8fdb7f);
 
 }
--- lib/views/kitchen.dart
+++ lib/views/kitchen.dart
@@ -1,15 +1,259 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import "package:sms/sms.dart";
 
-class KitchenInventory extends StatelessWidget {
+import 'package:firebase_database/firebase_database.dart';
+
+import 'package:smart_container/model/kitchenContainer.dart';
+import 'package:smart_container/assets/color.dart';
+import 'package:smart_container/views/kitchenDetail.dart';
+
+class KitchenInventory extends StatefulWidget {
+  @override
+  _KitchenInventoryState createState() => _KitchenInventoryState();
+}
+
+class _KitchenInventoryState extends State<KitchenInventory> {
+  final databaseRef = FirebaseDatabase.instance.reference().child("app");
+  List<KitchenContainer> kitCon = [];
+  // List<String>
+
+  @override
+  void initState() {
+    super.initState();
+    getFirebaseKitchenContainerData();
+  }
+
+  void _sendSMS(String msg, String recipients, String food) async {
+
+    SmsSender sender = new SmsSender();
+    String address = recipients;
+
+    SmsMessage message = new SmsMessage(address, msg);
+    sender.sendSms(message);
+    message.onStateChanged.listen((state) {
+      if (state == SmsMessageState.Sent) {
+        print("SMS is sent!");
+      } else if (state == SmsMessageState.Delivered) {
+        print("SMS is delivered!");
+        createAlertDialog(context, 'Success', 'The order has been placed for: ' + food);
+      }
+    });
+  }
+
+    void getFirebaseKitchenContainerData()  {
+    // kitCon.clear();
+    databaseRef
+        .child('container')
+        .child('kitchen')
+        .onValue
+        .listen((event) {
+      kitCon.clear();
+      var snapshot = event.snapshot;
+      var keys = snapshot.value.keys;
+      var vals = snapshot.value;
+      for (var key in keys) {
+        // use double.parse() incase data in firebase DB is string type
+        KitchenContainer k = new KitchenContainer(
+            vals[key]['type'], vals[key]['quantity'],
+            vals[key]['refill_quantity'], vals[key]['threshold'],
+            vals[key]['store'], vals[key]['store_price'],
+            vals[key]['last_refill'], vals[key]['auto_order'],
+          vals[key]['contact'],
+        );
+        kitCon.add(k);
+      }
+      setState(() { });
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Under Construction"),
-          ],
-        )
+
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("Kitchen Inventory"),
+        backgroundColor: AppColor.PRIMARY_LIGHT_GREEN,
+        toolbarOpacity: 1.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        child:
+          kitCon.length == 0
+            ?  Container(
+                  decoration: new BoxDecoration(
+                    color: AppColor.PRIMARY_WHITE,
+                  ),
+                  child: Center(
+                    child: SpinKitFadingCircle(
+                        color: AppColor.PRIMARY_GREEN,
+                        size: 150.0,
+                    ),
+                  ),
+          )
+              : ListView.builder(
+                  itemCount: kitCon.length ,
+                  itemBuilder:(BuildContext context, int index){
+                      return UI(
+                        kitCon[index]
+                      );
+                  }
+              ),
+      ),
     );
   }
+
+  Widget UI(KitchenContainer widget_kitCon){
+
+     String food_type = widget_kitCon.food_type;
+     double quantity = widget_kitCon.quantity;
+     double refill_quantity = widget_kitCon.refill_quantity;
+     bool autoOrder = widget_kitCon.auto_order;
+
+     if(widget_kitCon.quantity <=  widget_kitCon.threshold){
+       if(autoOrder == true){
+         String message = "Need to stock!";
+         // String recipents = "xxxxxxxxxx";
+         _sendSMS(message, widget_kitCon.contact, widget_kitCon.food_type);
+       }
+     }
+
+    return InkWell(
+      child: Container(
+          height:  MediaQuery.of(context).size.height * 0.27,
+          padding: EdgeInsets.only(top:10, left:10, right:10),
+              child: Card(
+                elevation: 10.0,
+                shadowColor: AppColor.PRIMARY_GREEN,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                color: AppColor.PRIMARY_WHITE,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                        backgroundColor: AppColor.PRIMARY_GREEN,
+                        radius: MediaQuery.of(context).size.width * 0.024,
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 41.0, left: 15.0),
+                            child:  Text(food_type, style: TextStyle(
+                            color: Colors.black,
+                            fontSize: MediaQuery.of(context).size.width * 0.1
+                            ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 7.0, left: 15.0),
+                            child: Text("Available: $quantity kg",style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: MediaQuery.of(context).size.width * 0.058
+                            ),
+                            )
+                            )
+                        ],
+                      ),
+                    Spacer(),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 0.0, left: 12.0, right: 14.0),
+                          child: Container(
+                              width: MediaQuery.of(context).size.width * 0.20,
+                              height:MediaQuery.of(context).size.height * 0.20,
+                              decoration: BoxDecoration(
+                                color: AppColor.PRIMARY_GREY,
+                                border: Border.all(color: AppColor.PRIMARY_BLACK, width: 3),
+                                borderRadius: BorderRadius.circular(9),
+                              ),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    bottom: 0,
+                                    child:  AnimatedContainer(
+                                        duration: Duration(seconds: 5),
+                                        width: MediaQuery.of(context).size.width * 0.20,
+                                        height: (quantity/refill_quantity) * MediaQuery.of(context).size.height * 0.20,
+                                        // color: AppColor.PRIMARY_GREEN,
+                                        curve: Curves.fastOutSlowIn,
+                                        decoration: BoxDecoration(
+                                          // todo: change color based on quantity! :P
+                                            borderRadius: BorderRadius.circular(9),
+                                            color: AppColor.PRIMARY_GREEN,
+                                        ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ),
+                        )
+                        ],
+                    ),
+              ],
+              ),
+              ),
+              ),
+          ),
+      onTap: () {
+        Navigator.push(context,
+            PageRouteBuilder(
+                transitionDuration: Duration(seconds: 1),
+                transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secAnimation, Widget child){
+                  animation = CurvedAnimation(parent: animation, curve: Curves.elasticInOut);
+                  return ScaleTransition(
+                    alignment: Alignment.center,
+                    scale: animation,
+                    child: child,
+                  );
+                },
+                pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secAnimation){
+                  return KitchenDetail(widget_kitCon);
+                }
+            )
+        );
+      },
+    );
+  }
+
+  // todo: implement push notifications using FCM.
+  createPushNotification(BuildContext context, String title, String msg) {
+
+  }
+
+  createAlertDialog(BuildContext context, String ttl, String msg) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(ttl),
+            content: Text(msg),
+            actions: <Widget>[
+              RaisedButton(
+                child: Text(
+                  'Ok',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          );
+        });
+  }
+
 }
